2b73aeb8ab9e60583b398e04cd6a0e26d2b8aee7
Jonathan McArthur <silverstagecoach@gmail.com>
2022-06-17 15:14:19 -0500

    SDLC Repo

--- sdlc.js
+++ sdlc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+const analysis = "Determining the best way to begin coding, and review new information";
+const design = "/Boots on the ground/ coding. In this step, whoever is being paid to work on the code will (or won't) take the ideas from the previous steps and /try/ to code it.";
+const implementation = "After coding something, you need your users to understand WTF they are looking at and how to use it. It doesn't matter that you put so many bells and whistles in MS Word, if no one knows about them.";
+const testing = "Time to break some stuff and put out fires.";
+const maintain = "Congrats! You've pushed out your code to the unwitting masses! Now, get your coding fire extinguisher, because someone /special/ is going to find out how to break your code, and it's your job to fix it (if you want more money).";
